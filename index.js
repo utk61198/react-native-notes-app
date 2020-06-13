154b3a5f1246ed115511cd3394b25af80aeb94ea
Utkarsh Sharma <utk61198@gmail.com>
2020-06-13 18:18:27 +0530

    database syncing

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import './app/components/polyfill'
 
 AppRegistry.registerComponent(appName, () => App);
